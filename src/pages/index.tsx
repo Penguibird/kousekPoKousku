f27f79db1f2e9f3bb5a7283913914a54a6fe5f1f
Vojtěch Loskot <Loskot@porg.cz>
2021-06-17 13:02:23 +0200

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,6 +21,7 @@ const AktualityCarousel = Loadable(() => import("../components/aktuality-carouse
 // const Map = Loadable(() => import('./../components/map'))
 import { useBreakpoint } from 'gatsby-plugin-breakpoints';
 import { useStaticQuery, graphql } from 'gatsby';
+import { useEffect } from 'react';
 
 const IndexPage = () => {
   const breakpoints = useBreakpoint();
@@ -44,15 +45,72 @@ const IndexPage = () => {
   `);
 
   let year = new Date().getFullYear() - 2011;
-  if (typeof year != "number" || year > 99) year = 10;
+  if (typeof year != "number" || year > 99) {
+    console.log("Error loading info from projekty db, using fallback value.", year);
+    year = 10;
+  }
 
   let pocetProjektu: number = data.allMarkdownRemark.edges.length;
-  if (typeof pocetProjektu != "number" || pocetProjektu < 90 || pocetProjektu > 999) pocetProjektu = 113;
+  if (typeof pocetProjektu != "number" || pocetProjektu < 90 || pocetProjektu > 999) {
+    console.log("Error loading info from projekty db, using fallback value.", pocetProjektu);
+    pocetProjektu = 113;
+  }
+
   const oneMillion = 1000000
-  const ceny: number = data.allMarkdownRemark.edges
+  let cena: number = data.allMarkdownRemark.edges
     .map((edge: any) => edge.node.frontmatter.price)
     .reduce((acc: number, val: number) => acc + val, 0)
-  const cenyText: string =   (ceny / oneMillion).to
+    ;
+  cena = (cena / oneMillion);
+  if (typeof cena != "number" || cena > 1000 || cena < 1) {
+    console.log("Error loading info from projekty db, using fallback value.", cena);
+    cena = 3.1;
+  }
+  let cenyText: string = cena.toFixed(1);
+
+  const approxeq = (v1: number, v2: number): boolean => {
+    const epsilon = 0.001;
+
+    return Math.abs(v1 - v2) < epsilon;
+  };
+
+  const animateValue = (obj: Element | null, start: number, end: number, duration: number, decimal: number = 0): void => {
+    if (window && obj) {
+      const stepperValueBoundary = decimal === 0
+        ? 1
+        : 1 / (decimal * 10);
+
+      let startTimestamp: number | null = null;
+      const step = (timestamp: number) => {
+        if (!startTimestamp) startTimestamp = timestamp;
+        const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+        obj.innerHTML = (progress * (end - start) + start).toFixed(decimal).toString();
+        if (decimal > 0) console.log((progress * (end - start) + start).toFixed(decimal).toString())
+        if (!approxeq(progress, 1)) {
+          window.requestAnimationFrame(step);
+        }
+      };
+      window.requestAnimationFrame(step);
+    }
+  }
+
+  const startValue = 0;
+  const animDuration = 1500;
+  useEffect(() => {
+    const observe = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          animateValue(document.getElementById('year'), startValue ?? 0, year, animDuration);
+          animateValue(document.getElementById('pocetProjektu'), startValue ?? 0, pocetProjektu, animDuration);
+          animateValue(document.getElementById('cenyText'), startValue ?? 0, Number.parseFloat(cenyText), animDuration, 1);
+        }
+      })
+    }, {});
+
+    const target = document.querySelector('section.cisla');
+    if (target) observe.observe(target);
+  }, [])
+
   return (
     <Layout >
       <LayerWrapper className="main-page-hero hero">
@@ -133,20 +191,20 @@ const IndexPage = () => {
           <li className="cislo roky">
             <svg className="img calendar" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none" /><path d="M9 11H7v2h2v-2zm4 0h-2v2h2v-2zm4 0h-2v2h2v-2zm2-7h-1V2h-2v2H8V2H6v2H5c-1.11 0-1.99.9-1.99 2L3 20c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 16H5V9h14v11z" /></svg>
             <p>
-              <span className="cislo">{year}</span>
+              <span className="cislo" id="year"></span>
               <span className="popis">let</span>
             </p>
           </li>
           <li className="cislo people">
             <svg className="img people" xmlns="http://www.w3.org/2000/svg" enableBackground="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><rect fill="none" height="24" width="24" /><g><path d="M12,12.75c1.63,0,3.07,0.39,4.24,0.9c1.08,0.48,1.76,1.56,1.76,2.73L18,18H6l0-1.61c0-1.18,0.68-2.26,1.76-2.73 C8.93,13.14,10.37,12.75,12,12.75z M4,13c1.1,0,2-0.9,2-2c0-1.1-0.9-2-2-2s-2,0.9-2,2C2,12.1,2.9,13,4,13z M5.13,14.1 C4.76,14.04,4.39,14,4,14c-0.99,0-1.93,0.21-2.78,0.58C0.48,14.9,0,15.62,0,16.43V18l4.5,0v-1.61C4.5,15.56,4.73,14.78,5.13,14.1z M20,13c1.1,0,2-0.9,2-2c0-1.1-0.9-2-2-2s-2,0.9-2,2C18,12.1,18.9,13,20,13z M24,16.43c0-0.81-0.48-1.53-1.22-1.85 C21.93,14.21,20.99,14,20,14c-0.39,0-0.76,0.04-1.13,0.1c0.4,0.68,0.63,1.46,0.63,2.29V18l4.5,0V16.43z M12,6c1.66,0,3,1.34,3,3 c0,1.66-1.34,3-3,3s-3-1.34-3-3C9,7.34,10.34,6,12,6z" /></g></svg>            <p>
-              <span className="cislo">112</span>
+              <span className="cislo" id="pocetProjektu"></span>
               <span className="popis">projektů</span>
             </p>
           </li>
           <li className="cislo penize">
             <svg className="img wallet" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none" /><path d="M10 16V8c0-1.1.89-2 2-2h9V5c0-1.1-.9-2-2-2H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-1h-9c-1.11 0-2-.9-2-2zm3-8c-.55 0-1 .45-1 1v6c0 .55.45 1 1 1h9V8h-9zm3 5.5c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5z" /></svg>
             <p>
-              <span className="cislo">3.1</span>
+              <span className="cislo" id="cenyText"></span>
               <span className="popis">miliónů Kč</span>
             </p>
           </li>
