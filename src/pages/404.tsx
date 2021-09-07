5fb287ef30cbb7b353bfc4a1c1c5f1a2f4896d72
Vojtech Loskot <loskotaklp@gmail.com>
2021-09-07 16:15:12 +0200

    404 fix

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,8 +5,10 @@ import Layout from "../components/layout"
 const NotFoundPage = () => {
   return (
     <Layout title="404 - Stránka nenalezena | Nadační fond Kousek po Kousku" >
-      <h1 style={{marginTop: '2em'}}>Stránka nenalezena</h1>
-      <Link to="/" className="button">Zpět na hlavní stránku</Link>
+      <div style={{margin: '1em', marginBottom: "2em"}}>
+        <h1 style={{marginTop: '3em'}}>Stránka nenalezena</h1>
+        <Link to="/" className="button">Zpět na hlavní stránku</Link>
+      </div>
     </Layout>
   )
 }
