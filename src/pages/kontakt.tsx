1f7bbd9d3b27151ad07780ff827c35446c0001eb
Saintboy13 <141815683+Saintboy13@users.noreply.github.com>
2025-02-11 11:39:14 +0100

    Update kontakt.tsx

--- src/pages/kontakt.tsx
+++ src/pages/kontakt.tsx
@@ -68,10 +68,10 @@ const KontaktPage: React.FC<KontaktPageProps> = ({ }) => {
                     <a href="tel:+420776003455">+420 776 003 455</a>
                 </li> */}
                 <li className="clovek">
-                    <h2 className="jmeno">Ivana Ježová</h2>
-                    <p className="popis">asistentka</p>
-                    <a href="mailto:info@kousekpokousku.cz">info@kousekpokousku.cz</a>
-                    <a href="tel:+420736426683">+420 736 426 683</a>
+                    <h2 className="jmeno">MgA. Magdaléna Feilhauerová</h2>
+                    <p className="popis">koordinátorka</p>
+                    <a href="mailto:magdalena@kousekpokousku.cz">magdalena@kousekpokousku.cz</a>
+                    <a href="tel:+420720768038">+420 720 768 038</a>
                 </li>
             </ul>
             <div className="kontakt">
@@ -98,7 +98,8 @@ const KontaktPage: React.FC<KontaktPageProps> = ({ }) => {
                         <p className="left">
                             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none" /><path d="M20.01 15.38c-1.23 0-2.42-.2-3.53-.56-.35-.12-.74-.03-1.01.24l-1.57 1.97c-2.83-1.35-5.48-3.9-6.89-6.83l1.95-1.66c.27-.28.35-.67.24-1.02-.37-1.11-.56-2.3-.56-3.53 0-.54-.45-.99-.99-.99H4.19C3.65 3 3 3.24 3 3.99 3 13.28 10.73 21 20.01 21c.71 0 .99-.63.99-1.18v-3.45c0-.54-.45-.99-.99-.99z" /></svg>
                         </p>
-                        <a href="tel:+420702022975">+420 702 022 975</a>
+                      {/*  <a href="tel:+420702022975">+420 702 022 975</a>
+                      */}
                     </div>
                 </div>
             </div>
